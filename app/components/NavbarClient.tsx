636758955b78192a365ac8f7cef96df794dd4020
sandizsz <sandissirmais8@gmail.com>
2025-03-06 23:44:23 +0200

    vercel fix error

--- app/components/NavbarClient.tsx
+++ app/components/NavbarClient.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { usePathname } from "next/navigation"
+
 import { Category, Casino } from "../utils/interface"
 import { useState, useEffect } from "react"
 import { GiftIcon } from "./GiftIcon"
