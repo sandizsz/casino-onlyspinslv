3b0e2932a6eec783d15ebcd3971bb1e610042e75
sandizsz <sandissirmais8@gmail.com>
2025-04-29 20:01:53 +0300

    commit fixes

--- app/components/CasinoComponent.tsx
+++ app/components/CasinoComponent.tsx
@@ -118,6 +118,8 @@ const CasinoComponent: React.FC<CasinoProps> = ({ casino, index, categorySlug })
                 alt={casino.offerTitle}
                 fill
                 className="object-cover rounded"
+                placeholder="blur"
+                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiMxRTFFMUUiLz48L3N2Zz4="
               />
             </div>
             <h3 className="text-sm font-semibold text-white">
